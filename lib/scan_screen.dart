c57819c6873a2601aa84889e47c7142c7b6736b9
sonomar <lawsonmarlowe@gmail.com>
2024-11-03 13:07:50 +0100

    add barcode scanner basic implementation

--- lib/scan_screen.dart
+++ lib/scan_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScanScreen extends StatelessWidget {
   const ScanScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Scan Screen'),
-    );
+    return SizedBox(
+        height: 400,
+        child: MobileScanner(onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          for (final barcode in barcodes) {
+            print(barcode.rawValue ?? "No Data found in QR");
+          }
+        }));
   }
 }
