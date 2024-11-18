0f39362bc5aaec4971992da4456b8b223f7defaf
sonomar <lawsonmarlowe@gmail.com>
2024-11-18 12:56:57 +0100

    add list to collectibles page and add asset storage

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,31 +4,30 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(32),
+    return const Padding(
+        padding: EdgeInsets.all(32),
         child: Column(
           children: [
-            const Row(children: [
+            Row(children: [
               Text('Hi Guest and',
                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700))
             ]),
-            const Row(children: [
+            Row(children: [
               Text('welcome to Deins',
                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700))
             ]),
             Row(children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 30.0),
-                color: Colors.grey[300],
+              Image(
+                image: AssetImage("assets/images/car2.jpg"),
                 width: 300.0,
                 height: 350.0,
               )
             ]),
-            const Row(children: [
+            Row(children: [
               Text('Kloppocar Asset',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700))
             ]),
-            const Padding(
+            Padding(
               padding: EdgeInsets.only(top: 10.0),
               child: Row(children: [
                 Text('Information about the asset',
