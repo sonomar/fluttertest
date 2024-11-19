7dfb9181d70089fb10657662626a6c6cfe4f52dd
sonomar <lawsonmarlowe@gmail.com>
2024-11-19 13:03:02 +0100

    collection pages add subpage code

--- lib/collection_screen.dart
+++ lib/collection_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import './collectibles/collection.dart';
 import 'dart:convert';
 
 // Fetch content from the json file
@@ -11,14 +12,14 @@ class CollectionScreen extends StatefulWidget {
 }
 
 class _CollectionScreenState extends State<CollectionScreen> {
-  List _items = [];
+  List _collections = [];
 
   Future<void> readJson() async {
     final String response =
-        await rootBundle.loadString('assets/json/example.json');
+        await rootBundle.loadString('assets/json/example_collection.json');
     final data = await json.decode(response);
     setState(() {
-      _items = data["collectibles"];
+      _collections = data["collections"];
     });
   }
 
@@ -34,14 +35,32 @@ class _CollectionScreenState extends State<CollectionScreen> {
   Widget build(BuildContext context) {
     return ListView(padding: const EdgeInsets.all(8), children: [
       Column(children: [
-        for (int i = 0; i < _items.length; i++) ...[
+        for (int i = 0; i < _collections.length; i++) ...[
           Row(children: [
-            Text(_items[i]["name"],
+            Text(_collections[i]["name"],
                 style:
                     const TextStyle(fontSize: 30, fontWeight: FontWeight.w700))
           ]),
-          Image(image: AssetImage(_items[i]["imageRef"])),
-          Text(_items[i]["description"],
+          InkWell(
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Collection(
+                        selectedCollection: _collections[
+                            i]) //here pass the actual values of these variables, for example false if the payment isn't successfull..etc
+                    ),
+              );
+            }, // Image tapped
+            splashColor: Colors.white10, // Splash color over image
+            child: Ink.image(
+              fit: BoxFit.cover, // Fixes border issues
+              width: 100,
+              height: 100,
+              image: AssetImage(_collections[i]["imageRef"]),
+            ),
+          ),
+          Text(_collections[i]["description"],
               style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w700))
         ]
       ])
