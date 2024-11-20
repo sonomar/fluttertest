a1064bd82471f3de22a1b0257096220184688971
sonomar <lawsonmarlowe@gmail.com>
2024-11-20 12:10:53 +0100

    add settings page-boxes

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -5,8 +5,30 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile Screen'),
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          height: 50,
+          child: const Center(child: Text('User Settings')),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          height: 50,
+          child: const Center(child: Text('Sign Up Today!')),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          height: 50,
+          child: const Center(child: Text('Terms & Conditions')),
+        ),
+        Container(
+          decoration: BoxDecoration(border: Border.all()),
+          height: 50,
+          child: const Center(child: Text('Privacy Policy')),
+        ),
+      ],
     );
   }
 }
