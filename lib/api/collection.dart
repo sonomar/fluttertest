832086736dabac2628f81ea09a410972d7d8d459
Lawson <lawsonmarlowe@gmail.com>
2025-04-24 13:15:20 +0200

    add api base for simpler GET API calls. POST etc still needed. Fixed bugs

--- lib/api/collection.dart
+++ lib/api/collection.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-import './apiInit.dart';
+import 'api_init.dart';
 
 void getAllCollectibles() async {
   apiGetRequest('/getAllCollectibles');
